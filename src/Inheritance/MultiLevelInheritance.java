c6a654c03147eb1d83f17c8382e70cba8f899c26
Ragul Kanna <ragulragi619@gmail.com>
2023-01-07 19:55:13 +0530

    MultiLevelInheritance

--- src/Inheritance/MultiLevelInheritance.java
+++ src/Inheritance/MultiLevelInheritance.java
@@ -1,8 +1,32 @@
 package Inheritance;
 
+class Car{
+  public void carName(){
+      System.out.println("Maruthi");
+  }
+}
+class Company extends Car{
+public void modelName(){
+    System.out.println("Swift");
+}
+}
+class Model extends Company{
+    public void topSpeed(){
+        System.out.println("90km");
+    }
+
+}
+
+
+
+
+
 public class MultiLevelInheritance {
     public static void main(String[] args){
-
+ Model s = new Model();
+ s.carName();
+ s.modelName();
+ s.topSpeed();
     }
 }
 
